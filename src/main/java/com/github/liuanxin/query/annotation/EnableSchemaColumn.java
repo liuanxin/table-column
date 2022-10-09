07863320f99cf6b00d87fdf0c73e2b656d8f0b76
Jie <liu_anxin@163.com>
2022-10-09 18:42:19 +0800

    fix

--- src/main/java/com/github/liuanxin/query/annotation/EnableSchemaColumn.java
+++ src/main/java/com/github/liuanxin/query/annotation/EnableSchemaColumn.java
@@ -1,6 +1,6 @@
 package com.github.liuanxin.query.annotation;
 
-import com.github.liuanxin.query.web.SchemaColumnController;
+import com.github.liuanxin.query.config.QuerySchemaInfoHandler;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
@@ -8,6 +8,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({SchemaColumnController.class})
+@Import({QuerySchemaInfoHandler.class})
 public @interface EnableSchemaColumn {
 }
